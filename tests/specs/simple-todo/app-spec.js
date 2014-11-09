2b7c6b087f6fd3c01179502b996f3ce2afe4e6de
Michel Mattos <mattos.michel@gmail.com>
2014-11-09 20:46:19 -0200

    Some tests for app.js
    Added some simple tests for app.js

--- tests/specs/simple-todo/app-spec.js
+++ tests/specs/simple-todo/app-spec.js
@@ -1,12 +1,36 @@
 'use strict';
 
 var m = require('../../../bower_components/mithril/mithril'),
-	app = require('../../../src/simple-todo/app');
+	app = require('./app-wrapper');
 
-describe('app.js', function(){
+describe('app.js', function() {
+
 	describe('> todo.Todo', function() {
-		it('should have "description" set to NULL and "done" set to FALSE by default', function() {
-			expect(false).toBe(true);
+		var todo;
+
+		beforeEach(function() {
+			todo = new app.Todo({description: "desc 01"});
 		});
+		
+		it('should have "done" set to FALSE by default', function() {
+			expect(todo.done()).toBe(false);
+		});
+		
+		it('should return the same description', function() {
+			expect(todo.description()).toBe("desc 01");
+		});
+		
+		it('should permit to change the description', function() {
+			todo.description("new desc");
+			
+			expect(todo.description()).toBe("new desc");
+		});
+		
+		it('should permit to be done', function() {
+			todo.done(true);
+			
+			expect(todo.done()).toBe(true);
+		});
+
 	});
 });
